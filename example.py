0e073d8ab7522214bc7530dafaa7a6e66c6430ae
amanraj270 <smartyamanraj27@gmail.com>
2026-06-17 16:03:12 +0530

    example of python

---
+++ example.py
@@ -0,0 +1,8 @@
+# Example Python script
+print("Hello, World!")
+print("This is an example script.")
+print("Python is fun!")
+print("Let's learn Python together.")
+print("Python is a versatile language.")
+print("You can use Python for web development, data analysis, and more.")
+print("Python has a large community and many libraries.")
